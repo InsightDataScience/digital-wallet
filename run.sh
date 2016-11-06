b5859c339bfd921c13b965fb07aab103e9b9f654
Hoa Nguyen <emhoa@users.noreply.github.com>
2016-11-06 13:21:17 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # but could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory paymo_input and output the files in the directory paymo_output
-python ./src/antifraud.py ./paymo_input/batch_payment.csv ./paymo_input/stream_payment.csv ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
+python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
